ef49929681fcb705d672080b0f809521fa68ad0e
Sean Murphy <sean@iamseanmurphy.com>
2026-08-17 13:03:28 +0000

    1.4.0

--- slider-block/dist/frontend/index.asset.php
+++ slider-block/dist/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'f5fd28e90965e48e3361');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '87ef359dcde27c2d8623');
